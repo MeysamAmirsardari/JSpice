54de1e3834579ed18abde7531cd0601738945fd6
Amir Soleimani <itsamirsoleimani@gmail.com>
2020-07-11 20:32:20 +0430

    Node modification (Added methods and properties to create Unions)

--- src/Node.java
+++ src/Node.java
@@ -3,15 +3,26 @@ import java.util.List;
 
 public class Node {
     public static Node ground;
-    public boolean added = false;
-    public double V;
+    public boolean isAdded = false;
+    public Node parentNode = null;
+    public double voltage;
     public int union;
     boolean internal = true;
+
     public List<Element> elementList = new ArrayList<Element>();
     public List<Node> adjacentNodes = new ArrayList<Node>();
-    public List<Double> voltage = new ArrayList<Double>();
+    public List<Double> voltageList = new ArrayList<Double>();
+
+    public String name;
+    // A property containing the Union they belong to
+
+    Node(String givenName) {
+        name = givenName;
+        voltage = 0;
+    }
 
     public double getVoltage() {
-        return (voltage.get(voltage.size() - 1));
+        return (voltageList.get(voltageList.size() - 1));
     }
+
 }
